5353030e38ab7572b30153a228a90ab29d37b592
Khaja Minhajuddin <minhajuddin@cosmicvent.com>
2011-03-20 19:52:42 +0530

    project#create stuff done

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,5 +1,26 @@
 class ProjectsController < ApplicationController
+  respond_to :html
+
   def index
     @projects = Project.all.to_a
+  end
+
+  def new
+    @project = Project.new
+  end
+
+  def create
+    @project = Project.new(params[:project])
+    if @project.save
+      flash[:notice] = 'Created project'
+      redirect_to projects_path
+    else
+      flash[:error] = 'Error while creating project'
+      render :action => :new
+    end
+  end
+
+  def show
+    @project = Project.find(params[:id])
   end
 end
--- spec/requests/projects_spec.rb
+++ spec/requests/projects_spec.rb
@@ -2,11 +2,22 @@ require 'spec_helper'
 
 describe "Projects" do
   describe "GET /projects" do
+
     it "should list existing projects" do
       Project.create! :name => "Khalid CMS"
       visit projects_path
       page.should have_content("Khalid CMS")
-      save_and_open_page
+      #save_and_open_page
     end
+
   end
+
+  it "should create a project" do
+    visit new_project_path
+    fill_in "Name", :with => "Taskr issue tracking"
+    click_button("Create")
+    page.should have_content("Taskr issue tracking")
+    #save_and_open_page
+  end
+
 end
